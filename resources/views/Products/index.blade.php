bd045b28d1bd838918b4b32b5a2ef0118c9229f1
rahdy <rahdy@code-rhapsodie.fr>
2023-05-24 17:27:58 +0200

    index

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -42,25 +42,6 @@
 
                             <button type="submit" class="btn btn-danger">Delete</button>
                         @endif
-                </form>
-                    <div class="col-md-4">
-                        <div id="product-{{ $product->id }}" class="single-product">
-                            <div class="part-2 col d-flex align-items-center flex-column">
-                                <h3 class="product-title">{{ $product->name }}</h3>
-                                <img src="{{ asset('images/'.$product->img) }}" alt="img">
-                                <h5 class="product-price">{{ $product->price }}</h5>
-                                <div>
-                                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
-                                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
-                                    <form action="{{ route('products.destroy',$product->id) }}" method="POST" class="d-inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Delete</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 @endforeach
             </div>
         </div>
