8d7dedbac3c44efcb06f6ec28b56aeda0fe3966b
rahdy <rahdy@code-rhapsodie.fr>
2023-05-24 17:42:45 +0200

    index csss

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -20,30 +20,32 @@
             @endif
             <div class="row mt-4">
                 @foreach ($products as $product)
-                    <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-                        <div class="col-md-6 col-lg-4 col-xl-3">
-                            <div id="product-1" class="single-product">
-                                <div class="part-2">
-                                    <h3 class="product-title">{{ $product->name }}</h3>
-                                    <a href="{{ route('products.show',$product->id) }}">
-                                        <img src="{{ asset('images/'.$product->img) }}" alt="img">
-                                    </a>
-                                    <h5 class="product-price">{{ $product->price }} €</h5>
+                    <div class="col-md-4">
+                        <div id="product-{{ $product->id }}" class="single-product">
+                            <div class="part-2 col d-flex align-items-center flex-column">
+                                <h3 class="product-title">{{ $product->name }}</h3>
+                                <a  href="{{ route('products.show',$product->id) }}">
+                                    <img src="{{ asset('images/'.$product->img) }}" alt="img">
+                                </a>
+                                <h5 class="product-price">{{ $product->price }}</h5>
+                                <div>
+                                    @if(Auth::check())
+
+                                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+                                    <form action="{{ route('products.destroy',$product->id) }}" method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                    @endif
+
                                 </div>
                             </div>
                         </div>
-
-
-                        @if(Auth::check())
-                            <a class="btn btn-primary" href="{{ route('products.edit', $product->id) }}">Edit</a>
-
-                            @csrf
-                            @method('DELETE')
-
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        @endif
+                    </div>
                 @endforeach
             </div>
+
         </div>
         <div class="pagination justify-content-center">
             {{ $products->links()}}
@@ -51,3 +53,4 @@
 
     </section>
 @endsection
+
