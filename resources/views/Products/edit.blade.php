83bedf9397e5f70ff20bbd1d48873e6074b1d59f
rahdy <rahdy@code-rhapsodie.fr>
2023-05-24 13:52:53 +0200

    crud product

--- resources/views/Products/edit.blade.php
+++ resources/views/Products/edit.blade.php
@@ -1,13 +1,12 @@
 @extends('base')
 
 @section('content')
-    <div class="container mt-4"></div>
-    <div class="row text-center">
-        <div class="col-lg-8 offset-lg-2">
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
             <div class="pull-left">
                 <h2>Edit Product</h2>
             </div>
-            <div class="pull-right text-right">
+            <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
             </div>
         </div>
@@ -41,20 +40,18 @@
                     <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $product->detail }}</textarea>
                 </div>
             </div>
-            <div class="col-md-8 offset-md-2 mt-2">
             <div class="form-group">
                 <strong>Prix:</strong>
                 <input type="number" name="price" value="{{ $product->price }}" class="form-control" placeholder="price">
             </div>
-            </div>
-            <div class="col-md-8 offset-md-2 mt-2">
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Image:</strong>
                     <input type="file" name="image" class="form-control" placeholder="image">
                     <img src="/images/{{ $product->image }}" width="300px">
                 </div>
             </div>
-
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
