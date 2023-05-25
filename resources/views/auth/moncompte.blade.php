1ed336ed71f295b14301e7e1d67c4a82dc4256f3
rahdy <rahdy@code-rhapsodie.fr>
2023-05-25 16:49:53 +0200

    template

--- resources/views/auth/moncompte.blade.php
+++ resources/views/auth/moncompte.blade.php
@@ -7,5 +7,22 @@
         <P> {{ $user->adress }}</P>
         <P>{{ $user->code_postal}}</P>
     </div>
-
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
