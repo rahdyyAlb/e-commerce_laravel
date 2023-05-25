e53fb703348c3ac160fecbd3895b2ea1e278adbd
rahdy <rahdy@code-rhapsodie.fr>
2023-05-25 15:10:43 +0200

    template

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,7 +9,7 @@
         @endif
 
         <div class="text-center">
-            <div class="row">
+            <div class="row-Product">
                 @foreach ($products as $product)
                     <div class="col-lg-3 col-md-6 mb-4 baskette" >
                         <div class="card" id="product-{{ $product->id }}">
